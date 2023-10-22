a76f9437849bc6d5f963bae6ace5fc0a24dd48f5
Carter <carterleebainter@gmail.com>
2023-10-22 17:43:30 -0400

    Initial commit

---
+++ src/extraCredit.java
@@ -0,0 +1,16 @@
+public class extraCredit {
+    public static void main(String[] args) {
+        for (int x = 5; x >= 1; x--) {
+            for (int y = 1; y <= 5; y++) {
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+        for (int x = 6; x >= 3; x--) {
+            for (int y = 6; y <= 9; y++) {
+                System.out.print("*");
+            }
+        }
+    }
+}
+
